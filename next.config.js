6ccb0871b52d3e30b338172579b0085cb2775f2e
thor-arisland <thor.arisland@ext.pexip.com>
2023-10-08 02:17:07 +0200

    img stuff

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true,
+    swcMinify: true,
+    images: {
+      loader: 'akamai',
+      path: '.',
+    }
 }
 
 module.exports = nextConfig
